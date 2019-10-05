38d6e9b95564866413a9a622868ef03dd034c4b8
kamlesh9070 <kamlesh9070@gmail.comm>
2019-10-05 22:32:02 +0530

    Menu with action

---
+++ Assets/DataFiles/Scripts/ChangeSceneMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeSceneMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void MenuScene()
+    {
+        Debug.Log("Inside AkramYouth");
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void AkramYouth()
+    {
+        Debug.Log("Inside AkramYouth");
+        SceneManager.LoadScene("AkramYouth");
+    }
+
+    // Update is called once per frame
+    public void PlayVideo()
+    {
+        Debug.Log("Inside PlayVideo");
+        SceneManager.LoadScene("Video");
+    }
+}
