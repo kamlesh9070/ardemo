4237729ff28ab3919d00a55a668b6d9740c4486b
Parth Gudhka <93parth@gmail.com>
2019-10-06 22:36:08 +0530

    Video overlay

---
+++ Assets/DataFiles/Scripts/VideoTrackableEventHandler.cs
@@ -0,0 +1,29 @@
+﻿/*===============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+ 
+Vuforia is a trademark of PTC Inc., registered in the United States and other
+countries.
+===============================================================================*/
+
+public class VideoTrackableEventHandler : DefaultTrackableEventHandler
+{
+    #region PROTECTED_METHODS
+
+    protected override void OnTrackingLost()
+    {
+        mTrackableBehaviour.GetComponentInChildren<VideoController>().Pause();
+
+        base.OnTrackingLost();
+    }
+    
+ 	protected override void OnTrackingFound()
+    {
+
+        base.OnTrackingFound();
+
+        mTrackableBehaviour.GetComponentInChildren<VideoController>().Play();
+        
+    }
+
+    #endregion // PROTECTED_METHODS
+}
